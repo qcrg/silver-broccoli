900513e0c04780cdda49cc4c98c018e43f305e10
qcrg <david-isupov@bk.ru>
2022-06-30 12:16:51 +0000

    fix common types

--- include/common.h
+++ include/common.h
@@ -1,6 +1,12 @@
 #ifndef _SILVER_BROCCOLI_COMMON_H
 #define _SILVER_BROCCOLI_COMMON_H
 
+typedef enum rtp_err_t_
+{
+    rtp_err_ok,
+    rtp_err_undefined
+} rtp_err_t;
+
 typedef unsigned int uint_;
 
 #endif//_SILVER_BROCCOLI_COMMON_H
